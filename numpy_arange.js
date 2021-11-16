246c336b18c81d9b5bf2910bc86a2ecbcc3df7cd
RXXXBNUer <92851207+RXXXBNUer@users.noreply.github.com>
2021-11-16 17:35:26 +0800

    Add files via upload

---
+++ numpy_arange.js
@@ -0,0 +1,13 @@
+var $builtinmodule = function (name) {
+	var mod = {__name__: new Sk.builtin.str("numpy")}
+	numpy.arange = new Sk.builtin.func(function(start,stop,step,dtype) {
+		start=Sk.ffi.remapToJs(start);
+		stop=Sk.ffi.remapToJs(stop);
+		step=Sk.ffi.remapToJs(step);
+		var arr = new Array[];
+		for(i=start;i<stop;i+=dtype)arr.push(i);
+		
+        return Sk.ffi.remapToPy(arr);
+    });
+	return mod;
+}
