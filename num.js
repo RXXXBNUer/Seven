e5803d54e70d980310316248e1440d0fb2c4fc18
RXXXBNUer <92851207+RXXXBNUer@users.noreply.github.com>
2021-11-16 17:50:13 +0800

    Add files via upload

---
+++ num.js
@@ -0,0 +1,9 @@
+var $builtinmodule = function (name) {
+	var numpy = {__name__: new Sk.builtin.str("numpy")}
+	numpy.sin = new Sk.builtin.func(function(a) {
+		a=Sk.ffi.remapToJs(a);
+		for(i=0;i<a.length;i++)a[i]=Math.sin(a[i]);
+        return Sk.ffi.remapToPy(a);
+    });
+	return numpy;
+}
