19c2bfca58cdf811000d099ed4858d3e8797e5dc
RXXXBNUer <92851207+RXXXBNUer@users.noreply.github.com>
2022-05-27 18:26:44 +0800

    Add files via upload

---
+++ skdemo.js
@@ -0,0 +1,9 @@
+var $builtinmodule = function (name) {
+	var demo = {__name__: new Sk.builtin.str("demo")}
+	demo.move = new Sk.builtin.func(function(a) {
+		a=Sk.ffi.remapToJs(a);
+		move(a);
+        return Sk.ffi.remapToPy(a);
+    });
+	return numpy;
+}
