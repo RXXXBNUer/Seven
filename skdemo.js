9beb32761dc8bd11e0c044afdd8416519c08ebb3
RXXXBNUer <92851207+RXXXBNUer@users.noreply.github.com>
2022-05-27 18:47:27 +0800

    Update skdemo.js

--- skdemo.js
+++ skdemo.js
@@ -1,7 +1,6 @@
 var $builtinmodule = function (name) {
 	var demo = {__name__: new Sk.builtin.str("demo")}
 	demo.move = new Sk.builtin.func(function() {
-		print('1');
 		x-=4;
 		player.position.set(x, y);
         return 0;
