15d61331c5e2663245169c266e91b3f1c93c30ab
RXXXBNUer <92851207+RXXXBNUer@users.noreply.github.com>
2022-05-27 19:14:32 +0800

    Update skdemo.js

--- skdemo.js
+++ skdemo.js
@@ -18,26 +18,27 @@ var $builtinmodule = function (name) {
 		};
 		player.position.set(x, y);
         return Sk.ffi.remapToPy(mode);
-    }
-	demo.scale = new Sk.builtin.func(function(mode) {
-		mode=Sk.ffi.remapToJs(mode);
-		switch(mode){
-			case 1:
-			player.scale.x += 0.1;
-			player.scale.y += 0.1;
-			break;
-			case 2:
-			player.scale.x -= 0.1;
-			player.scale.y -= 0.1;
-			break;
-			case 3:
-			player.scale.x *= -1;
-			break;
-			case 4:
-			player.scale.y *= -1;
-			break;
-		};
-        return Sk.ffi.remapToPy(mode);
-    });
+//     }
+// 	demo.scale = new Sk.builtin.func(function(mode) {
+// 		mode=Sk.ffi.remapToJs(mode);
+// 		switch(mode){
+// 			case 1:
+// 			player.scale.x += 0.1;
+// 			player.scale.y += 0.1;
+// 			break;
+// 			case 2:
+// 			player.scale.x -= 0.1;
+// 			player.scale.y -= 0.1;
+// 			break;
+// 			case 3:
+// 			player.scale.x *= -1;
+// 			break;
+// 			case 4:
+// 			player.scale.y *= -1;
+// 			break;
+// 		};
+//         return Sk.ffi.remapToPy(mode);
+//     }
+		);
 	return demo;
 }
